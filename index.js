a69cf5ae618f3b359713c92817b4f6e2f7bd3a2e
attrenate <nikolozghomidzevol100@gmail.com>
2025-04-26 03:36:16 +0400

    racxa1

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ const person = {
   function doubleEvens(arr) {
     return arr.map(num => num % 2 === 0 ? num * 2 : num);
   }
+  function logObject(obj) {
+    // Get key-value pairs as an array
+    const entries = Object.entries(obj);
+    
+    // Loop through each key-value pair
+    for (let [key, value] of entries) {
+      console.log(`${key} is ${value}`);
+    }
+  }
+  
+  // Example usage
+  const user = { name: "Alex", age: 20 };
+  logObject(user);
+  // Output:
+  // "name is Alex"
+  // "age is 20"
   
